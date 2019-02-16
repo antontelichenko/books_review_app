71429953ad3e27bb2611d6de5b5918cdabc1ead4
Telichenko Anton <geka412315@gmail.com>
2019-02-16 15:15:09 +0200

    build version added

---
+++ client/build/precache-manifest.5582cd9315a6ff01ce83adc1a24f7c37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ea4c2e267ac150e39bb",
+    "url": "/static/js/main.6ea4c2e2.chunk.js"
+  },
+  {
+    "revision": "01eb1c65b455f32ec51a",
+    "url": "/static/js/1.01eb1c65.chunk.js"
+  },
+  {
+    "revision": "45a118a353444d599e19cf8ee33e37e6",
+    "url": "/index.html"
+  }
+];
