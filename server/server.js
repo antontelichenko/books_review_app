250fd1a759bf89de7d708cc01f17e00db7208700
Telichenko Anton <geka412315@gmail.com>
2019-02-16 15:12:39 +0200

    server added

--- server/server.js
+++ server/server.js
@@ -157,6 +157,13 @@ app.delete('/api/delete_book',(req,res)=>{
     })
 })
 
+if(process.env.NODE_ENV==='production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
+
 const port = 3001;
 app.listen(port,()=>{
     console.log(`server running`);
